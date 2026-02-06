3b20be11f3c7906e0b411ae60acf41f85f998acf
yasmeenasif <zaheeryasmeen05@gmail.com>
2026-02-06 22:54:03 +0530

    modified file

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("CRM Backend is running!");
+});
+
+
 // Routes
 app.use("/api/auth", require("./app/routes/authRoutes"));
 app.use("/api/customers", require("./app/routes/customerRoutes"));
