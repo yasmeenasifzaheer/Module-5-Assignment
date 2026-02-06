3e4bfb89c656d16cd76044cbfb8e2970f8531c6e
yasmeenasif <zaheeryasmeen05@gmail.com>
2026-02-06 23:53:54 +0530

    final commit

--- app/controllers/caseController.js
+++ app/controllers/caseController.js
@@ -1,10 +1,16 @@
 const Case = require("../models/Case");
 
 exports.createCase = async (req, res) => {
-  const newCase = new Case(req.body);
-  await newCase.save();
-  res.json(newCase);
+  try {
+    console.log("Request body:", req.body); // debug input
+    const newCase = await Case.create(req.body);
+    res.json(newCase);
+  } catch (err) {
+    console.error(err); // log exact error
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
+
 
 exports.updateCase = async (req, res) => {
   const updated = await Case.findByIdAndUpdate(req.params.id, req.body, { new: true });
