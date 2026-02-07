086a45b04139a8166c5282565d7ff7cdb00c89d0
yasmeenasif <zaheeryasmeen05@gmail.com>
2026-02-07 09:27:31 +0530

    modified file

--- app/models/Case.js
+++ app/models/Case.js
@@ -1,11 +1,27 @@
 const mongoose = require("mongoose");
 
 const CaseSchema = new mongoose.Schema({
-  customer_id: { type: mongoose.Schema.Types.ObjectId, ref: "Customer" },
-  assigned_to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  priority: String,
-  status: String,
-  created_at: { type: Date, default: Date.now }
+  customer_id: {
+    type: String,
+    required: true
+  },
+  assigned_to: {
+    type: String,
+    required: true
+  },
+  priority: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("Case", CaseSchema);
+
